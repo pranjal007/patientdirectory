85f9948841df55747c03fb16c31acb4929a33cda
pranjal007 <bahuguna.pranjal@gmail.com>
2017-10-08 20:13:43 +0530

    Second Commit

--- config/database.js
+++ config/database.js
@@ -1,7 +1,29 @@
-const crypto = require('crypto').randomBytes(256).toString('hex');
+
+/*const crypto = require('crypto').randomBytes(256).toString('hex');
 
 module.exports = {
   uri: process.env.databaseUri, // Databse URI and database name
   secret: crypto, // Cryto-created secret
   db: process.env.databaseName // Database name
+}*/
+
+/*
+const crypto = require('crypto').randomBytes(256).toString('hex');
+
+module.exports = {
+    url : "mongodb://localhost:27017/patientui",
+    secret : crypto,
+    dbname : "patientui"
 }
+*/
+
+const crypto = require('crypto').randomBytes(256).toString('hex');
+
+module.exports = {
+    url : 'mongodb://pranjal007:speak1234@ds113825.mlab.com:13825/patientui',
+    secret : crypto,
+    dbname : "patientui"
+}
+
+
+//mongodb://<dbuser>:<dbpassword>@ds113825.mlab.com:13825/patientui
